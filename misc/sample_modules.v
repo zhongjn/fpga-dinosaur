1384c285fc5b957ffb44429d10c8a5327b71b5f8
xipotatonium <xipotatonium@outlook.com>
2019-01-06 18:02:35 +0800

    Game模块

--- misc/sample_modules.v
+++ misc/sample_modules.v
@@ -20,12 +20,99 @@ module Game(
     input wire game_clk, // 60Hz左右
     input wire jump, // 跳跃
     input wire start, // 游戏开始
-    output reg [11:0] dino_y,
-    output reg [11:0] obstacle_x,
-    output reg night,
-    output reg game_over,
-    output reg [15:0] score
+    output reg [11:0] dino_y,       // 恐龙脚的y
+    output reg [11:0] obstacle_x,   // 障碍物x
+    // output reg night,               // 1表示晚上
+    output reg game_over            // 1表示游戏结束
+    // output reg [15:0] score         // 分数，二进制
 );
+    localparam DINO_INIT_V = 123123111;     // 恐龙跳起时的速度
+    localparam DINO_G = 1231;               // 重力加速度
+    localparam OBSTACLE_INIT_X = 123121;    // 障碍物初始位置
+    
+    integer speed;
+    // reg can_inc_score;
+    reg is_going;
+    // 初始化，游戏暂停
+    initial begin is_going <= 0; end
+
+    // 控制夜晚的变化，比如每32分变一次？
+    // assign night = score[4];
+
+    always @ (posedge game_clk)
+    begin
+        if (is_going == 0 && start == 1)
+        begin
+            // 初始化游戏
+            dino_y <= GROUND_SCREEN_Y;
+            obstacle_x <= OBSTACLE_INIT_X;
+            // night <= 0;
+            game_over <= 0;
+            // score <= 15'b0;
+            speed <= 0;
+            is_going <= 1;
+            // can_inc_score <= 1;
+        end
+
+        // 控制碰撞
+        if ((
+                DINO_SCREEN_X >= obstacle_x - OBSTACLE_BITMAP_CENTER_X
+                && DINO_SCREEN_X <= obstacle_x - OBSTACLE_BITMAP_CENTER_X + OBSTACLE_BITMAP_SIZE_X
+                && dino_y >= GROUND_SCREEN_Y - OBSTACLE_BITMAP_CENTER_Y
+                && dino_y <= GROUND_SCREEN_Y - OBSTACLE_BITMAP_CENTER_Y + OBSTACLE_BITMAP_SIZE_Y
+            )
+            ||(
+                DINO_SCREEN_X + DINO_HEAD_OFFSET_X >= obstacle_x - OBSTACLE_BITMAP_CENTER_X
+                && DINO_SCREEN_X + DINO_HEAD_OFFSET_X <= obstacle_x - OBSTACLE_BITMAP_CENTER_X + OBSTACLE_BITMAP_SIZE_X
+                && dino_y + DINO_HEAD_OFFSET_Y >= GROUND_SCREEN_Y - OBSTACLE_BITMAP_CENTER_Y
+                && dino_y + DINO_HEAD_OFFSET_Y <= GROUND_SCREEN_Y - OBSTACLE_BITMAP_CENTER_Y + OBSTACLE_BITMAP_SIZE_Y
+            ))
+        begin
+            is_going <= 0;
+            game_over <= 1;
+        end
+
+        if (is_going == 1)
+        begin
+            // 控制恐龙的跳跃
+            if (jump == 1 && dino_y == GROUND_SCREEN_Y)
+            begin
+            // 只有在jump为1和在地上的时候才起跳
+                speed = DINO_INIT_V;
+            end
+            else if (dino_y > GROUND_SCREEN_Y && speed != 0)
+            begin
+            // 空中不断修改y值和速度
+                dino_y = dino_y + speed;
+                speed = speed - DINO_G;
+            end
+            else if (dino_y <= GROUND_SCREEN_Y)
+            begin
+            //落地后结束跳跃
+                speed <= 0;
+                dino_y <= GROUND_SCREEN_Y;
+            end
+
+            // 障碍物固定速度左移(速度暂定为1，需要保证obstacle_x会变成0)
+            if (obstacle_x > 0)
+            begin
+                obstacle_x = obstacle_x - 1;
+            end
+            else
+            begin
+                obstacle_x = OBSTACLE_INIT_X;
+            end
+
+            // 加分
+            /*
+            if (can_inc_score == 1 && DINO_SCREEN_X > obstacle_x)
+            begin
+                can_inc_score <= 0;
+                score <= score + 1;
+            end
+            */
+        end
+    end
 endmodule
 
 
@@ -52,7 +139,7 @@ module Vga(
 	output reg [11:0] screen_x,
 	output reg [11:0] screen_y,
     output wire [11:0] cur_x,
-    output wire [11:0] cur_y,
+    output wire [11:0] cur_y
 );
 endmodule
 
