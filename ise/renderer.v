9ded271fb041c1d8bbaa990d98001e7c5bdab14f
zhongjn <2389206378@qq.com>
2019-01-07 19:55:00 +0800

    vga模块和renderer模块

--- ise/renderer.v
+++ ise/renderer.v
@@ -1,20 +1,81 @@
 `include parameters.v
 
+// 注意：绘制会被降采样
+// 屏幕只有320*240大小
 module Renderer(
     input wire [11:0] dino_y,
+    input wire [1:0] dino_state,
     input wire [11:0] obstacle_x,
-    input wire game_over,
+    input wire night,
     input pixel_clk, // 像素绘制时钟
-    input [11:0] cur_x, // 当前像素位置X
-	input [11:0] cur_y, // 当前像素位置Y
+    input [11:0] vga_x, // 当前像素位置X
+	input [11:0] vga_y, // 当前像素位置Y
     output reg pixel // 当前像素是否点亮，点亮为1
 );
 
+    wire [11:0] screen_x = vga_x >> 1; // 除以二，降采样
+    wire [11:0] screen_y = vga_y >> 1; // 除以二，降采样
+
 
     always @(posedge pixel_clk) begin
-        wire is_dino, is_obstacle, is_game_over;
-        // TODO: 判断是否是恐龙或者仙人掌，或者是游戏结束图标
-        pixel <= is_dino || is_obstacle || is_game_over;
+
+        wire dino_in_rect, dino_rel_x, dino_rel_y;
+        InRect dino_rect(
+            .cur_x(screen_x), 
+            .cur_y(screen_y), 
+            .rect_pos_x(DINO_SCREEN_X - DINO_BITMAP_CENTER_X),
+            .rect_pos_y(dino_y - DINO_BITMAP_CENTER_Y),
+            .rect_size_x(DINO_BITMAP_SIZE_X),
+            .rect_size_y(DINO_BITMAP_SIZE_Y),
+            .in_rect(dino_in_rect),
+            .rel_x(dino_rel_x),
+            .rel_y(dino_rel_y)
+        );
+        wire dino_pixel_pos = 
+            dino_state * DINO_BITMAP_SIZE_X * DINO_BITMAP_CENTER_Y + 
+            dino_rel_y * DINO_BITMAP_SIZE_X + 
+            dino_rel_x;
+        wire is_dino = dino_in_rect ? DINO_IMG[dino_pixel_pos] : 0;
+
+
+        wire obs_in_rect, obs_rel_x, obs_rel_y;
+        InRect obs_rect(
+            .cur_x(screen_x), 
+            .cur_y(screen_y), 
+            .rect_pos_x(obstacle_x - OBSTACLE_BITMAP_CENTER_X),
+            .rect_pos_y(GROUND_SCREEN_Y - OBSTACLE_BITMAP_CENTER_Y),
+            .rect_size_x(OBSTACLE_BITMAP_SIZE_X),
+            .rect_size_y(OBSTACLE_BITMAP_SIZE_Y),
+            .in_rect(obs_in_rect),
+            .rel_x(obs_rel_x),
+            .rel_y(obs_rel_y)
+        );
+        wire obs_pixel_pos = 
+            obs_rel_y * obs_BITMAP_SIZE_X + 
+            obs_rel_x;
+        wire is_obstacle = obs_in_rect ? obs_IMG[obs_pixel_pos] : 0;
+
+
+        pixel <= night != (is_dino || is_obstacle); // 夜晚时反色，用异或
     end
 
+endmodule
+
+
+module InRect(
+    input wire [11:0] cur_x, cur_y, 
+    input wire [11:0] rect_pos_x, rect_pos_y,
+    input wire [11:0] rect_size_x, rect_size_y,
+    output wire in_rect,
+    output wire [11:0] rel_x, rel_y // valid when in rect
+);
+    assign in_rect = 
+        cur_x >= rect_pos_x && 
+        cur_y >= rect_pos_y &&
+        cur_x < rect_pos_x + rect_size_x &&
+        cur_y < rect_pos_y + rect_size_y;
+    
+    assign rel_x = cur_x - rect_pos_x;
+    assign rel_y = cur_y - rect_pos_y;
+    
 endmodule
--- ise/vga.v
+++ ise/vga.v
@@ -1,14 +1,52 @@
-module Vga(
-	input wire pixel_clk,
-	input wire pixel,
-	output reg [7:0] RED,
-	output reg [7:0] GREEN,
-	output reg [7:0] BLUE,
-	output reg HSYNC,
-	output reg VSYNC,
-	output reg [11:0] screen_x,
-	output reg [11:0] screen_y,
-    output wire [11:0] cur_x,
-    output wire [11:0] cur_y
-);
+module vga(
+    input vga_clk, pixel
+    output reg [11:0] row_addr, col_addr,
+    output reg [3:0] r, g, b
+    output reg hs, vs); // vgac
+
+    wire clrn = 1'b1;
+
+   // h_count: VGA horizontal counter (0-799)
+   reg [11:0] h_count; // VGA horizontal counter (0-799): pixels
+   always @ (posedge vga_clk) begin
+       if (!clrn) begin
+           h_count <= 12'h0;
+       end else if (h_count == 12'd799) begin
+           h_count <= 12'h0;
+       end else begin 
+           h_count <= h_count + 12'h1;
+       end
+   end
+   // v_count: VGA vertical counter (0-524)
+   reg [11:0] v_count; // VGA vertical   counter (0-524): lines
+   always @ (posedge vga_clk or negedge clrn) begin
+       if (!clrn) begin
+           v_count <= 120'h0;
+       end else if (h_count == 12'd799) begin
+           if (v_count == 12'd524) begin
+               v_count <= 12'h0;
+           end else begin
+               v_count <= v_count + 12'h1;
+           end
+       end
+   end
+   
+    // signals, will be latched for outputs
+    wire  [11:0] row    =  v_count - 12'd35;
+    wire  [11:0] col    =  h_count - 12'd143;
+    wire        h_sync = (h_count > 12'd95);    //  96 -> 799
+    wire        v_sync = (v_count > 12'd1);     //   2 -> 524
+
+    wire c = pixel ? 4'b0000 : 4'b1111;
+
+    // vga signals
+    always @ (posedge vga_clk) begin
+        row_addr <=  row;
+        col_addr <=  col;
+        hs       <=  h_sync;   // horizontal synchronization
+        vs       <=  v_sync;   // vertical   synchronization
+        r        <=  c;
+        g        <=  c;
+        b        <=  c;
+    end
 endmodule
